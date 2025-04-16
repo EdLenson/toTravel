56072c724c6d5d498f73e31f20b895763e475268
Eduard Vybornov <vei95@icloud.com>
2025-04-16 12:01:11 +0300

    Готовая с детальной информацией о стране

---
+++ toTravel/IATAcodes.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// IATA-коды для столиц
+let capitalIataCodes: [String: String] = [
+    "Tokyo": "TYO",
+    "Moscow": "MOW",
+    "Washington, D.C.": "IAD",
+    "Paris": "PAR",
+    "London": "LON",
+    "Berlin": "BER",
+    "Beijing": "PEK",
+    "New Delhi": "DEL",
+    "Canberra": "CBR",
+    "Brasilia": "BSB",
+    // Добавь другие столицы по мере необходимости
+]
